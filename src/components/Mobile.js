a880cd375e7564a9e839916afeced3961d439581
samkes244 <82683164+samkes244@users.noreply.github.com>
2021-05-06 01:17:49 +0530

    Mobile updated
    Commit second made up everything in home page

--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -2,24 +2,71 @@ import React from "react";
 import "./Product.css";
 
 const Mobile = () => {
-  const movies =[
-     { name: "Redmi Note 10",
+  const movies = [
+    {
+      name: "Redmi Note 10",
       img: "https://m.media-amazon.com/images/I/41atHSWSGaL.jpg",
       price: "$160",
       Rating: "⭐⭐⭐⭐⭐",
     },
-    
-  ]
-  
+  ];
+
   return (
     <div>
-   { movies ? <h2>{movies.map((movie=>{
-    return(
-    <h2>{movie.name}</h2>
-    )
-
-    }))}
-    </h2>: <h2>loading</h2> }
+      {movies ? (
+        movies.map((movie) => (
+          <div className="product">
+            <div>
+              <div className="border">
+                <h2>{movie.name}</h2>
+                <img className="images" src={movie.img} alt="" />
+                <strong>
+                  <p>{movie.price}</p>
+                </strong>
+                <p>{movie.Rating}</p>
+                <button>See Items</button>
+              </div>
+            </div>
+            <div>
+              <div className="border">
+                <h2>{movie.name}</h2>
+                <img className="images" src={movie.img} alt="" />
+                <strong>
+                  <p>{movie.price}</p>
+                </strong>
+                <p>{movie.Rating}</p>
+                <button>See Items</button>
+              </div>
+            </div>
+            <div>
+              <div className="border">
+                <h2>{movie.name}</h2>
+                <img className="images" src={movie.img} alt="" />
+                <strong>
+                  <p>{movie.price}</p>
+                </strong>
+                <p>{movie.Rating}</p>
+                <button>See Items</button>
+              </div>
+            </div>
+            <div>
+              <div className="border">
+                <h2>{movie.name}</h2>
+                <img className="images" src={movie.img} alt="" />
+                <strong>
+                  <p>{movie.price}</p>
+                </strong>
+                <p>{movie.Rating}</p>
+                <button>See Items</button>
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      )}
     </div>
   );
 };
